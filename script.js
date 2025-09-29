3257cf580303673319d6a6bdb5b8bd0551fe45f0
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-09-29 15:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let userResponse = confirm("Do you want to proceed?");
+
+function confirm() {
+	if(userResponse){
+			alert(“You clicked OK. Proceeding...”);
+	}else{
+		alert("You clicked Cancel. Exiting...");
+	}
+}
